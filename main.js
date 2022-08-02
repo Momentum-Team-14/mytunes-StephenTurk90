1d7f5ecf5af9107c503712497df8ebefa3215188
StephenTurk90 <stephenjturk@gmail.com>
2022-08-02 16:21:37 -0400

    updated main.js to include code to find albums

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+const container = document.querySelector(".container")
+
 let resultsDiv = document.querySelector('#results')
 console.log('results div', resultsDiv)
 
+
+// fetch songs from iTunes
 let searchUrl = ' https://itunes.apple.com/search?term=Andrew+Bird'
 
 fetch(searchUrl, {
@@ -13,9 +17,13 @@ fetch(searchUrl, {
 })
 // data is whatever the above code returns
 .then(function (data) {
-    let songs = data.results
-    console.log(songs)
-    for (let song of songs){
-        
-    }
+    let searchResults = data.results
+    console.log(searchResults)
 })
+// function for displaying song results
+function showSongName(songArray) {
+    for (let song of songArray){
+        let songReturnDiv = document.createElement('div')
+        songReturnDiv.classList.add('songReturn')
+    }
+}
