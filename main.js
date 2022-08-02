8f3387659470748a9289dcda9f8165511d0e1509
StephenTurk90 <stephenjturk@gmail.com>
2022-08-02 17:03:28 -0400

    created a div to show artist name

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const container = document.querySelector(".container")
 
 let resultsDiv = document.querySelector('#results')
-console.log('results div', resultsDiv)
-
 
 // fetch songs from iTunes
 let searchUrl = ' https://itunes.apple.com/search?term=Andrew+Bird'
@@ -19,11 +17,14 @@ fetch(searchUrl, {
 .then(function (data) {
     let searchResults = data.results
     console.log(searchResults)
+    showArtistName(searchResults)
 })
 // function for displaying song results
-function showSongName(songArray) {
+function showArtistName(songArray) {
     for (let song of songArray){
         let songReturnDiv = document.createElement('div')
         songReturnDiv.classList.add('songReturn')
+        resultsDiv.append(songReturnDiv); 
+        songReturnDiv.append(song.artistName);
     }
 }
