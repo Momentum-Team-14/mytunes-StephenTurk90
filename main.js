aee81e606cd7f6a9200d67304076cd01c3e7a13d
StephenTurk90 <stephenjturk@gmail.com>
2022-08-03 12:48:02 -0400

    added search function

--- main.js
+++ main.js
@@ -3,48 +3,61 @@ const container = document.querySelector(".container")
 let resultsDiv = document.querySelector('#results')
 
 // fetch songs from iTunes
-let searchUrl = ' https://itunes.apple.com/search?term=Andrew+Bird'
+let searchBaseUrl = 'https://itunes.apple.com/search?term='
 
-let searchForm = 
+let searchForm = document.querySelector('#search-form')
 
-fetch(searchUrl, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json'}
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let searchBox = document.querySelector('#search-box')
+    let searchUrl = `${searchBaseUrl}${searchBox.value}`
+    console.log('search url', searchUrl)
+    getSearchResults(searchUrl)
 })
-// response is whatever the fetch returns
-.then(function (response) {
-    return response.json()
-})
-// data is whatever the above code returns
-.then(function (data) {
-    let searchResults = data.results
-    console.log(searchResults);
-    showResults(searchResults);
-})
-// functions for displaying song results
-function showResults(songArray) {
-    for (let song of songArray){
-        let songReturnDiv = document.createElement('div')
-        songReturnDiv.classList.add('songReturn')
-        
-        let albumCoverImg = document.createElement('img');
-        albumCoverImg.classList.add('albumCoverImg')
-        albumCoverImg.src = song.artworkUrl100
-        // album cover attempt
 
-        let songTitleDiv = document.createElement('div');
-        songTitleDiv.classList.add('songTitle')
-        songTitleDiv.append(song.trackName);
-        // song title return
-        
-        let artistReturnDiv = document.createElement('div');
-        artistReturnDiv.classList.add('artistReturn')
-        artistReturnDiv.append(song.artistName);
-        // artist return
+function getSearchResults(url) {
+    fetch(url, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    })
+    // response is whatever the fetch returns
+    .then(function (response) {
+        return response.json()
+    })
+    // data is whatever the above code returns, in this case response.json()
+    .then(function (data) {
+        let searchResults = data.results
+        console.log(searchResults);
+        showResults(searchResults);
+    })
 
-        resultsDiv.appendChild(songReturnDiv)
-        songReturnDiv.appendChild(albumCoverImg)
-        songReturnDiv.appendChild(songTitleDiv)
-        songReturnDiv.appendChild(artistReturnDiv)
+    
+    // functions for displaying song results
+    function showResults(songArray) {
+        for (let song of songArray){
+            let songReturnDiv = document.createElement('div')
+            songReturnDiv.classList.add('songReturn')
+            
+            let albumCoverImg = document.createElement('img');
+            albumCoverImg.classList.add('albumCoverImg')
+            albumCoverImg.src = song.artworkUrl100
+            // album cover attempt
+            
+            let songTitleDiv = document.createElement('div');
+            songTitleDiv.classList.add('songTitle')
+            songTitleDiv.append(song.trackName);
+            // song title return
+            
+            let artistReturnDiv = document.createElement('div');
+            artistReturnDiv.classList.add('artistReturn')
+            artistReturnDiv.append(song.artistName);
+            // artist return
+            
+            resultsDiv.appendChild(songReturnDiv)
+            songReturnDiv.appendChild(albumCoverImg)
+            songReturnDiv.appendChild(songTitleDiv)
+            songReturnDiv.appendChild(artistReturnDiv)
+        }
     }
+    
 }
