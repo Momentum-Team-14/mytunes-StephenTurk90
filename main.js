0f1ed642b4fffe7e5637f9547cd87f937e239b5a
StephenTurk90 <stephenjturk@gmail.com>
2022-08-04 12:32:39 -0400

    no results found

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container")
 let resultsDiv = document.querySelector('#results')
 
 // fetch songs from iTunes
-let searchBaseUrl = 'https://itunes.apple.com/search?term='
+let searchBaseUrl = 'https://proxy-itunes-api.glitch.me/search?term='
 
 let searchForm = document.querySelector('#search-form')
 
@@ -25,9 +25,9 @@ function getSearchResults(url) {
     // response is whatever the fetch returns
     .then(function (response) {
         if (!response.ok) {
-            throw Error(response.type);
+            throw Error(response);
         } else {
-            console.log(response.type)
+            console.log(response)
         return response.json()
     }
     })
@@ -46,37 +46,45 @@ function getSearchResults(url) {
 
     // functions for displaying song results
     function showResults(songArray) {
-        for (let song of songArray){
-            let songReturnDiv = document.createElement('div')
-            songReturnDiv.classList.add('songReturn')
-            
-            let albumCoverImg = document.createElement('img');
-            albumCoverImg.classList.add('albumCoverImg')
-            albumCoverImg.src = song.artworkUrl100
-            // album cover attempt
-            
-            let songTitleDiv = document.createElement('div');
-            songTitleDiv.classList.add('songTitle')
-            songTitleDiv.append(song.trackName);
-            // song title return
-            
-            let artistReturnDiv = document.createElement('div');
-            artistReturnDiv.classList.add('artistReturn')
-            artistReturnDiv.append(song.artistName);
-            // artist return
+        if (songArray.lenghth > 0) {
 
-            let audioTag = document.createElement("audio");
-            audioTag.classList.add("audio");
-            audioTag.src = song.previewUrl;
-            audioTag.controls = true;
-            songReturnDiv.appendChild(audioTag);
             
-            resultsDiv.appendChild(songReturnDiv)
-            songReturnDiv.appendChild(albumCoverImg)
-            songReturnDiv.appendChild(songTitleDiv)
-            songReturnDiv.appendChild(artistReturnDiv)
-            songReturnDiv.appendChild(audioTag)
-            //shows alls data 
+            for (let song of songArray){
+                let songReturnDiv = document.createElement('div')
+                songReturnDiv.classList.add('songReturn')
+                
+                let albumCoverImg = document.createElement('img');
+                albumCoverImg.classList.add('albumCoverImg')
+                albumCoverImg.src = song.artworkUrl100
+                // album cover attempt
+                
+                let songTitleDiv = document.createElement('div');
+                songTitleDiv.classList.add('songTitle')
+                songTitleDiv.append(song.trackName);
+                // song title return
+                
+                let artistReturnDiv = document.createElement('div');
+                artistReturnDiv.classList.add('artistReturn')
+                artistReturnDiv.append(song.artistName);
+                // artist return
+                
+                let audioTag = document.createElement("audio");
+                audioTag.classList.add("audio");
+                audioTag.src = song.previewUrl;
+                audioTag.controls = true;
+                songReturnDiv.appendChild(audioTag);
+                
+                resultsDiv.appendChild(songReturnDiv)
+                songReturnDiv.appendChild(albumCoverImg)
+                songReturnDiv.appendChild(songTitleDiv)
+                songReturnDiv.appendChild(artistReturnDiv)
+                songReturnDiv.appendChild(audioTag)
+                //shows alls data 
+            }
+        } else {
+            let noResults = document.createElement('p');
+            noResults.innerText = "No results found"
+            resultsDiv.appendChild(noResults)
         }
     }
 
